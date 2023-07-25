48feceade0235dc56ba40435992822028b659264
Steven Yan <yunsheng.yan@walmart.com>
2023-07-24 19:48:40 -0700

    Add post-installation support for helm chart installation

--- start.sh
+++ start.sh
@@ -3,3 +3,8 @@ vagrant up
 
 # reload rook-ceph-operator
 vagrant ssh k8s-m-1 -c "kubectl -n rook-ceph delete po -l app=rook-ceph-operator --force"
+vagrant ssh k8s-m-1 -c "kubectl -n rook-ceph delete po -l app=rook-ceph-tools"
+
+sleep 360
+
+vagrant ssh k8s-m-1 -c "helm install myprometheus /tmp/prometheus.tar.gz"
